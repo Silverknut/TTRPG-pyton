082a110170a0e5368d39afdb11f855fd99776661
Silverknut <70891876+Silverknut@users.noreply.github.com>
2025-09-26 18:07:07 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ game_state = {
     "players": {}
 }
 
-async def handler(websocket, path):
+async def handler(websocket):
     # Добавляем клиента
     connected.add(websocket)
     print(f"Клиент подключился. ID: {id(websocket)}")
